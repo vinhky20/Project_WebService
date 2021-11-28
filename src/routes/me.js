880e3a2c9ea27062209aac40ce426b8125d9285b
vinhky20 <nguyenvinhky26@gmail.com>
2021-11-28 11:22:21 +0700

    Edit interface

--- src/routes/me.js
+++ src/routes/me.js
@@ -13,5 +13,6 @@ router.get('/trash/posts', meController.trashPosts);
 // Nếu có thêm url liên quan tới me thì viết vào đây
 
 module.exports = router;
+
 // tới đây cho mỗi route trong folder routes
 // Chỉ thay đổi một số thông tin cho phù hợp, vd như tên controller
